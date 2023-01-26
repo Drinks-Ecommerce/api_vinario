a508ac8debd280de6c33d54df3c8a3316976af22
AgusCorcico <corcicoagustin@gmail.com>
2023-01-26 11:55:59 -0300

    deploy

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const app = require('./src/app.js')
 const { conn } = require('./src/db.js');
+const port = process.env.PORT || 3000;
 
 
 
-conn.sync({ force: true }).then(() => {   // si tenes en true, renueva la base de datos
+conn.sync({ force: false }).then(() => {   // si tenes en true, renueva la base de datos
 
-  app.listen(3000, () => {
-    console.log('%s listening at 3000'); // eslint-disable-line no-console
+  app.listen(port, () => {
+    console.log(`%s listening at ${port}`); // eslint-disable-line no-console
   });
 });
